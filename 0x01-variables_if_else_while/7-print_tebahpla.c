6e7d195fff18cd9b237acb652d43a994b5bcd82e
andressantamaria <andressantamariac@gmail.com>
2022-02-11 17:42:18 -0500

    -print_tebahpla.c file

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Prints the alphabet in reverse order 
+ * Return: integer zero.
+ */
+int main(void)
+{
+	int c;
+
+	for (c = 122; c > 96; c--)
+		putchar(c);
+
+	putchar('\n');
+	return (0);
+}
