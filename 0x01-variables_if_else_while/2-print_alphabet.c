507f6ed4bd97e77d71b5fb49a00b202639b0102f
andressantamaria <andressantamariac@gmail.com>
2022-02-11 15:12:16 -0500

    2-print_alphabet.c file

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Prints alphabet in lower case
+ * Return: integer zero
+ */
+int main(void)
+{
+	int c;
+
+	for (c = 97; c < 123; c++)
+	{
+		putchar(c);
+	}
+	printf("\n");
+
+	return (0);
+}
