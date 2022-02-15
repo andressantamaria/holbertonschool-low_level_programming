426e557771d73e3c00dfa9de116471c226f0b8e3
andressantamaria <andressantamariac@gmail.com>
2022-02-15 18:05:00 -0500

    10-add.c file

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add - add two integers
+ * @n1: integer for addition
+ * @n2: integer for addition
+ * Return: addition result
+ */
+int add(int n1, int n2)
+{
+	int suma = n1 + n2;
+
+	return (suma);
+}
