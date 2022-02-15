6c1414a6890cb26a080f3c70d461d6ae68a662b0
andressantamaria <andressantamariac@gmail.com>
2022-02-15 18:55:13 -0500

    11-print_to_98.c file

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - Prints numbers from n to 98, followed by a new line.
+ * @n: Where to start de count
+ * Description: prints in 24 H format
+ */
+void print_to_98(int n)
+{
+	int aux;
+
+	if (n <= 98)
+	{
+
+		for (aux = n; aux <= 98; aux++)
+		{
+			if (aux != 98)
+				printf("%d ,", aux);
+			else
+				printf("%d ", aux);
+		}
+	}
+	else
+	{
+		for (aux = n; aux >= 98; aux--)
+		{
+			if (aux != 98)
+				printf("%d ,", aux);
+			else
+				printf("%d ", aux);
+		}
+	}
+
+	printf("\n");
+}
