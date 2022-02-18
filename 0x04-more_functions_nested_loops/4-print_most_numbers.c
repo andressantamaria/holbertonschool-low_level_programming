5d37c2aae41c9a87f6174da7288705413601594f
andressantamaria <andressantamariac@gmail.com>
2022-02-17 23:15:31 -0500

    4-print_most_numbers.c betty fixed

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,15 +1,16 @@
 #include "main.h"
 /**
- * print_most_number -  Prints from 0 to 9, neither 2 non 4
+ * print_most_numbers - Prints from 0 to 9, neither 2 non 4
  */
 void print_most_numbers(void)
 {
 	int i;
+
 	for (i = 48; i < 58; i++)
 	{
 		if (i != 50 && i != 52)
 			_putchar(i);
 	}
 
-        _putchar('\n');
+	_putchar('\n');
 }
