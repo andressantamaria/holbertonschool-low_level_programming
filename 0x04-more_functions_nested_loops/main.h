c2e00bf09820d5d47be7d65746ed669bbfff30e5
andressantamaria <andressantamariac@gmail.com>
2022-02-17 22:55:14 -0500

    main.h file

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,16 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <unistd.h>
+#include <stdio.h>
+
+void  _putchar(char c);
+int _isupper(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+
+
+#endif
