e42d0f59117ef6b80cc5b5bad69f5c5b582294d9
andressantamaria <andressantamariac@gmail.com>
2022-02-17 23:57:42 -0500

    7-print_diagonal.c loop fixed

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_diagonal - draws a diagonal
+ * @n: determine lenght
+ */
+void print_diagonal(int n)
+{
+	int j,k;
+	if(n > 0)
+	{
+		for(j = 0; j < n; j++)
+		{
+			for(k=0; k<j; k++)
+			{
+				_putchar(32);
+			}
+			_putchar(92);
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
