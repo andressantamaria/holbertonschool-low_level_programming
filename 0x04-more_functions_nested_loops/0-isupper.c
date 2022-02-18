6e822cf1977056f39e26584fc5841b0986556aec
andressantamaria <andressantamariac@gmail.com>
2022-02-17 22:31:41 -0500

    0-isupper.c file

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _isupper - Checks if int is UPPER
+ * @c: int to evaluated
+ * Return: int value
+ */
+int _isupper(int c)
+{
+	if (c <= 65 && c <= 89)
+		return (1);
+	else
+		return (0);
+}
