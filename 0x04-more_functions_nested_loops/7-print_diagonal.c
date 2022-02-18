5964bcbfa1a761d92fed8c3460ee43e3dc01a93b
andressantamaria <andressantamariac@gmail.com>
2022-02-18 00:02:35 -0500

    7-print_diagonal.c betty

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -5,12 +5,13 @@
  */
 void print_diagonal(int n)
 {
-	int j,k;
-	if(n > 0)
+	int j, k;
+
+	if (n > 0)
 	{
-		for(j = 0; j < n; j++)
+		for (j = 0; j < n; j++)
 		{
-			for(k=0; k<j; k++)
+			for (k = 0; k < j; k++)
 			{
 				_putchar(32);
 			}
